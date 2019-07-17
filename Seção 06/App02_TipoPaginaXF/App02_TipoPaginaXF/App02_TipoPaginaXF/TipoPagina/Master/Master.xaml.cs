d4e5103c3e593590271f44aa14da00d414a2eadc
Amauri Junior <supremo.neo@hotmail.com>
2019-07-17 10:15:23 -0300

    Projeto Tipo De Telas - Finalizado

---
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Master/Master.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TipoPagina.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnPagina1_Clicked(object sender, EventArgs e)
+        {
+            Detail = new Navigation.Pagina1();
+            IsPresented = false;
+        }
+
+        private void BtnPagin2_Clicked(object sender, EventArgs e)
+        {
+            Detail = new Navigation.Pagina2();
+            IsPresented = false;
+        }
+
+        private void BtnConteudo_Clicked(object sender, EventArgs e)
+        {
+            Detail = new Conteudo();
+            IsPresented = false;
+        }
+    }
+}

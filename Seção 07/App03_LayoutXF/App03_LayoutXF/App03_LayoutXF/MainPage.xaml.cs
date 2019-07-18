ecd97542257ce646693ac2e263b74735aadf5aff
Amauri Junior <supremo.neo@hotmail.com>
2019-07-18 10:31:41 -0300

    Seção 06 - Layout

---
+++ Seção 07/App03_LayoutXF/App03_LayoutXF/App03_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App03_LayoutXF
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnScroll_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+        }
+
+        private void BtnRelative_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+
+        private void BtnAbsolute_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+
+        private void BtnGrid_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+
+        private void BtnStack_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+    }
+}

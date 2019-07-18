ef6015507907105f585891183033a5f7861a2f3f
Amauri Junior <amauri.junior.1989@gmail.com>
2019-07-18 11:53:40 -0300

    Seção 07 - Tela Master

---
+++ Seção 07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_Xamarin.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void BtnSobreXamarin_Clicked(object sender, EventArgs e)
+        {
+            
+            Detail = new NavigationPage(new Pages.Xamarin());
+            IsPresented = false;
+
+        }
+
+        private void BtnMiguel_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Perfil01());
+            IsPresented = false;
+        }
+    }
+}

abec696cf053b2b67d017828b477cedbf2655de9
Amauri Junior <amauri.junior.1989@gmail.com>
2019-07-26 10:53:48 -0300

    Seção 10 - Estilos

---
+++ Seção 10/App01_Estilo/App01_Estilo/App01_Estilo/Menu/Master.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_Estilo.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStyle_Clicked(object sender, EventArgs e)
+        {
+            //Detail =
+        }
+
+        private void BtnImplicitStylePage_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.ImplicitStylePage());
+            IsPresented = false;
+        }
+
+        private void BtnExplicitStylePage_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.ExplicitStylePage());
+            IsPresented = false;
+        }
+
+        private void BtnGlobalStylePage_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.GlobalStylePage());
+            IsPresented = false;
+        }
+    }
+}
